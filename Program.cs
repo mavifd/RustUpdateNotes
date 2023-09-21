bff799109f1b7538fa11e4a570fdd700ae214870
ferhatdayanmavi <123773891+ferhatdayanmavi@users.noreply.github.com>
2023-09-21 14:58:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -34,11 +34,9 @@ namespace RustTurkiye_Responder
 
             _client.MessageReceived += MessageReceived;
 
-            // İlk başta bir sonraki güncelleme zamanını hesaplayın
             CalculateNextUpdateTimestamp();
 
-            // Timer'ı başlatarak her dakika bir sonraki güncelleme zamanını yeniden hesaplayın
-            Timer timer = new Timer(60_000); // 60,000 milisaniye = 1 dakika
+            Timer timer = new Timer(60_000);
             timer.Elapsed += (sender, e) => CalculateNextUpdateTimestamp();
             timer.Start();
 
@@ -47,48 +45,50 @@ namespace RustTurkiye_Responder
 
         private void CalculateNextUpdateTimestamp()
         {
-            // Şu anki zamanı alın
+
+            //bugün 21:00 timestamp alma
             DateTime bugun = DateTime.Today;
+            DateTime saat21 = bugun.AddHours(21);
+            long bugüntimestamp = ((DateTimeOffset)saat21).ToUnixTimeSeconds();
 
-            // Eğer bu ayın ilk perşembesi saat 21:00'den önce ise, bu ayın ilk perşembesini hesaplayın
-            if (DateTime.Now.Hour < 21)
+            //ayın ilk perşembesi 21:00 timestamp alma
+            DateTime suAn = DateTime.Now;
+            DateTime ilkGun = new DateTime(suAn.Year, suAn.Month, 1);
+            int gunDegeri = (int)ilkGun.DayOfWeek;
+            DateTime ilkPersembe = ilkGun.AddDays((4 - gunDegeri + 7) % 7);
+            DateTime saat21ilkay = ilkPersembe.AddHours(21);
+            long ayinilkpersembesitimestamp = ((DateTimeOffset)saat21ilkay).ToUnixTimeSeconds();
+
+
+            //loglama
+            Console.WriteLine(bugüntimestamp.ToString());
+            Console.WriteLine(ayinilkpersembesitimestamp.ToString());
+
+
+            //ilk persembe geçildi mi?
+            if(bugüntimestamp > ayinilkpersembesitimestamp) 
             {
-                DateTime buAyinIlkPersembesi = new DateTime(bugun.Year, bugun.Month, 1);
-                while (buAyinIlkPersembesi.DayOfWeek != DayOfWeek.Thursday)
-                {
-                    buAyinIlkPersembesi = buAyinIlkPersembesi.AddDays(1);
-                }
-                buAyinIlkPersembesi = buAyinIlkPersembesi.AddHours(21);
-
-                //TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-                //DateTime cetZamani = TimeZoneInfo.ConvertTimeFromUtc(buAyinIlkPersembesi.ToUniversalTime(), cet);
-                //bool yazZamani = cet.IsDaylightSavingTime(cetZamani);
-                //if (yazZamani)
-                //{
-                //    buAyinIlkPersembesi = buAyinIlkPersembesi.AddHours(1);
-                //}
-
-                _nextUpdateTimestamp = new DateTimeOffset(buAyinIlkPersembesi).ToUnixTimeSeconds();
-            }
-            else
-            {
-                // Eğer bu ayın ilk perşembesi saat 21:00'den sonra ise, bir sonraki ayın ilk perşembesini hesaplayın
+                //ilk perşembe geçildi
+                Console.WriteLine("ilk perşembe geçilmiş");
                 DateTime birSonrakiAyinIlkPersembesi = new DateTime(bugun.Year, bugun.Month, 1).AddMonths(1);
                 while (birSonrakiAyinIlkPersembesi.DayOfWeek != DayOfWeek.Thursday)
                 {
                     birSonrakiAyinIlkPersembesi = birSonrakiAyinIlkPersembesi.AddDays(1);
                 }
                 birSonrakiAyinIlkPersembesi = birSonrakiAyinIlkPersembesi.AddHours(21);
-                TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-                DateTime cetZamani = TimeZoneInfo.ConvertTimeFromUtc(birSonrakiAyinIlkPersembesi.ToUniversalTime(), cet);
-                bool yazZamani = cet.IsDaylightSavingTime(cetZamani);
-
-                if (yazZamani)
-                {
-                    birSonrakiAyinIlkPersembesi = birSonrakiAyinIlkPersembesi.AddHours(1);
-                }
-
                 _nextUpdateTimestamp = new DateTimeOffset(birSonrakiAyinIlkPersembesi).ToUnixTimeSeconds();
+            }
+            else 
+            {
+                //ilk perşembe geçilmedi
+                Console.WriteLine("ilk perşembe geçilmemiş");
+                DateTime buAyinIlkPersembesi = new DateTime(bugun.Year, bugun.Month, 1);
+                while (buAyinIlkPersembesi.DayOfWeek != DayOfWeek.Thursday)
+                {
+                    buAyinIlkPersembesi = buAyinIlkPersembesi.AddDays(1);
+                }
+                buAyinIlkPersembesi = buAyinIlkPersembesi.AddHours(21);
+                _nextUpdateTimestamp = new DateTimeOffset(buAyinIlkPersembesi).ToUnixTimeSeconds();
             }
         }
 
@@ -96,29 +96,18 @@ namespace RustTurkiye_Responder
         {
             if (message.Channel.Id == _channelId)
             {
-                if (message.Author.Id != _client.CurrentUser.Id) // Botun kendi mesajlarını kontrol et
+                if (message.Author.Id != _client.CurrentUser.Id) 
                 {
                     if (message.Content.ToLower().Contains("wipe") || message.Content.ToLower().Contains("güncelleme") || message.Content.ToLower().Contains("global"))
                     {
                         IUser user = message.Author;
                         string userTag = $"{user.Mention}";
 
-                        // Şu anki zamanı Central European Standard Time (CET) zaman dilimine göre hesaplayın
-                        TimeZoneInfo cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-                        DateTime cetZamani = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cet);
-
                         EmbedBuilder embedBuilder = new EmbedBuilder();
                         embedBuilder.WithTitle(":information_source:  **Güncelleme Bilgisi**  :information_source:");
                         embedBuilder.WithDescription("`Her ayın ilk perşembesi (Yaz Dönemi 21:00 - Kış Dönemi 22:00) gelen güncelleme ile tüm sunuculara` ***Zorunlu Harita Sıfırlaması*** `atılır. BP Sıfırlaması ise sunucu sahibinin isteğine bağlıdır.` ");
                         embedBuilder.WithThumbnailUrl("https://yt3.googleusercontent.com/ytc/AL5GRJUOzRJWMKDaDQdJVVsXHCBcWQsOZYe3YZOfTj1k=s176-c-k-c0x00ffffff-no-rj-mo");
                         embedBuilder.WithFooter(DateTime.Now.ToString(), "https://cdn.discordapp.com/attachments/1060075799081918516/1072987687730032670/logo.png");
-
-                        // Eğer şu anki zaman 21:00'dan sonra ise, bir sonraki güncelleme zamanını bir sonraki ayınkini hesaplayın
-                        if (cetZamani.Hour >= 21 && cetZamani.Month == DateTimeOffset.FromUnixTimeSeconds(_nextUpdateTimestamp).DateTime.Month)
-                        {
-                            CalculateNextUpdateTimestamp();
-                        }
-
                         embedBuilder.AddField("Sonraki Güncelleme Tarihi", $"<t:{_nextUpdateTimestamp}:F>", false);
                         embedBuilder.AddField("Sonraki Güncellemeye Kalan Zaman", $"<t:{_nextUpdateTimestamp}:R>", false);
                         embedBuilder.AddField("Soran Kullanıcı", userTag, false);
