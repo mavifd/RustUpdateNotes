7662a4afb4111d764bc3a163e12452d36afd011c
Mavi <123773891+ferhatdayan-src@users.noreply.github.com>
2024-06-18 14:39:44 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -819,11 +819,12 @@ namespace RT_Control
         {
             await UpdateVersionIfNeeded(MainL, Main_Public, ":radioactive: **Oyuncular için yeni bir Güncelleme geldi!** :radioactive:", "Güncellemeyi görmüyorsanız, steaminizi yeniden başlatmanız gerekiyor.", true, v => Main_Public = v);
             await UpdateVersionIfNeeded(ServerPublicL, Server_Public, ":radioactive: **Sunucular için yeni bir Güncelleme geldi!** :radioactive:", "Sunucu sahipleri, sunucularını güncelleyebilirler.", true, v => Server_Public = v);
-            await UpdateVersionIfNeeded(ServerStagingL, Server_Staging, "", "**Sunucu** taraflı Rust Staging güncellemesi.", false, v => Server_Staging = v);
-            await UpdateVersionIfNeeded(ServerAuxL, Server_Aux02, "", "**Sunucu** taraflı Rust Staging - **Aux02** güncellemesi.", false, v => Server_Aux02 = v);
-            await UpdateVersionIfNeeded(StagingPublicL, Staging_Public, "", "**İstemci** taraflı Rust Staging - **Public** güncellemesi.", false, v => Staging_Public = v);
-            await UpdateVersionIfNeeded(StagingMainL, Staging_Main, "", "**İstemci** taraflı Rust Staging - **Main** güncellemesi.", false, v => Staging_Main = v);
-            await UpdateVersionIfNeeded(StagingAuxL, Staging_Aux02, "", "**İstemci** taraflı Rust Staging - **Aux02** güncellemesi.", false, v => Staging_Aux02 = v);
+          
+            await UpdateVersionIfNeeded(ServerStagingL, Server_Staging, "", "**Sunucu** taraflı **Rust Staging** güncellemesi.", false, v => Server_Staging = v);
+            await UpdateVersionIfNeeded(ServerAuxL, Server_Aux02, "", "**Sunucu** taraflı **Rust Staging** - **Aux02** güncellemesi.", false, v => Server_Aux02 = v);
+            await UpdateVersionIfNeeded(StagingPublicL, Staging_Public, "", "**İstemci** taraflı **Rust Staging** - **Public** güncellemesi.", false, v => Staging_Public = v);
+            await UpdateVersionIfNeeded(StagingMainL, Staging_Main, "", "**İstemci** taraflı **Rust Staging** - **Main** güncellemesi.", false, v => Staging_Main = v);
+            await UpdateVersionIfNeeded(StagingAuxL, Staging_Aux02, "", "**İstemci** taraflı **Rust Staging** - **Aux02** güncellemesi.", false, v => Staging_Aux02 = v);
         }
 
         private static async Task SendUpdateMessage(string title, string message, string changemsg, bool everyone)
@@ -846,7 +847,7 @@ namespace RT_Control
                     if (channel == null) continue;
                     if (!await CheckBotPerms(guild) || !await CheckChannelPerms(channel)) { await NoPermsSendMessage(guild, "(Güncelleme Takip Kanalı)"); continue; };
                     if (everyone) await channel.SendMessageAsync("@everyone", false, embedBuilder.Build());
-                    else if (guild.Id == _MainDiscord && channel.Id == 1243032097099350029) await channel.SendMessageAsync($"<@{_PingMavi}>", false, embedBuilder.Build());
+                    else if (guild.Id == _MainDiscord && channel.Id == 1243032097099350029) await channel.SendMessageAsync($"<@1252567993653792809", false, embedBuilder.Build());
                     else { await channel.SendMessageAsync("", false, embedBuilder.Build()); }
                 }
             }
