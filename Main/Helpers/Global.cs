2f26692e93171fe30e3c791e296e0f13fa78184f
mavifd <123773891+mavifd@users.noreply.github.com>
2025-04-18 16:10:20 +0300

    Update Global.cs

--- Main/Helpers/Global.cs
+++ Main/Helpers/Global.cs
@@ -6,15 +6,15 @@ namespace RustUpdateNotes.GlobalClass
 {
     public static class Global
     {
-        public static readonly string Token = "MTExMTAxNjg2MDc0NjUzMDgzNg.G5Tmp_.pk-mcC5NNneSCwWOZuvFwOzovem4rieLdLKT3k";
+        public static readonly string Token = "-";
 
         public static DiscordSocketClient Client;
 
-        public static DiscordWebhookClient DiscordLog = new DiscordWebhookClient("https://discord.com/api/webhooks/1243163905660817539/lBmr8EQHh1xvVu-32u1W_cjbdyneoB4Rd271orjMWrPiXY6BQTfpkSjCtfLpnJkYE_bE");
+        public static DiscordWebhookClient DiscordLog = new DiscordWebhookClient("-");
 
-        public static readonly ulong MainDiscordSohbet = 1223037877911556110;
-        public static readonly ulong Mavi = 170569747497222145;
-        public static readonly ulong MainDiscordID = 1223037877911556107;
+        public static readonly ulong MainDiscordSohbet = 0;
+        public static readonly ulong Mavi = 0;
+        public static readonly ulong MainDiscordID = 0;
 
         public static Dictionary<ulong, List<ulong>> UpdateNoteChannels = new Dictionary<ulong, List<ulong>>();
         public static Dictionary<ulong, List<ulong>> UpdateDateChannels = new Dictionary<ulong, List<ulong>>();
